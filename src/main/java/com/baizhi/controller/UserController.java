f251ccd68b3ecdd8532aa2fc967be1855ce64cb0
htf <743375282@qq.com>
2019-02-22 15:53:46 +0800

    环境搭建

---
+++ src/main/java/com/baizhi/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.baizhi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("user")
+public class UserController {
+    @RequestMapping("/quick")
+    public Object test() {
+        System.out.println("测试成功");
+        Map<String, String> map = new HashMap<>();
+        map.put("hehe", "1234");
+        return map;
+    }
+}
